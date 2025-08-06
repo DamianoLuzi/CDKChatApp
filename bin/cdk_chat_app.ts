705e5b5929eb54f9f89a0f66dc764d193d395426
Damiano Luzi <luzi.dami@gmail.com>
2025-08-06 17:01:04 +0200

    empty pipeline re-deployment from scratch

--- bin/cdk_chat_app.ts
+++ bin/cdk_chat_app.ts
@@ -3,7 +3,7 @@ import * as cdk from 'aws-cdk-lib';
 import { CdkPipelineStack } from '../lib/cdk-pipeline-stack';
 
 const app = new cdk.App();
-new CdkPipelineStack(app, 'CdkPipelineStack', {
+new CdkPipelineStack(app, 'CdkChatAppPipelineStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
